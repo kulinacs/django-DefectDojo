2193a6de1221f861ada67a7c9b5cd6582e668a40
Nicklaus McClendon <nicklaus@kulinacs.com>
2018-08-02 23:25:27 -0400

    Break Docker container into individual processes

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+python manage.py makemigrations dojo
+python manage.py makemigrations --merge --noinput
+python manage.py migrate
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
+python manage.py runserver 0.0.0.0:8000
